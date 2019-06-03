d10a2cdcd14a5d6d78d3be008872c9765bc14a43
Yuhang Song <yuhangsong.china@gmail.com>
2019-06-03 21:26:14 +0100

    ParticleSystem

---
+++ Assets/ArenaSDK/ThirdPartyAssets/ParticleSystem/TutorialInfo/Scripts/Readme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class Readme : ScriptableObject {
+	public Texture2D icon;
+	public string title;
+	public Section[] sections;
+	public bool loadedLayout;
+    //6756ed7b80223ac4e8f026ce27541de4
+    [Serializable]
+	public class Section {
+		public string heading, text, linkText, url;
+	}
+}
