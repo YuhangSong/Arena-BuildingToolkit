6bdd2e6d586fd277952f6cb91f6690e74fa3e07b
Yuhang Song <yuhangsong.china@gmail.com>
2019-06-07 19:07:55 +0100

    SportCar

---
+++ Assets/ArenaSDK/ThirdPartyAssets/SportCar/Scripts/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    
+    private bool MouseClick = false;
+    public float cameraRotateSpeed = 5f;
+    public Transform RotateTarget;
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Rotation
+        if (Input.GetMouseButtonDown(0)) MouseClick = true;
+        if (Input.GetMouseButtonUp(0)) MouseClick = false;
+
+        if(MouseClick) transform.RotateAround(RotateTarget.position, RotateTarget.up, Input.GetAxis("Mouse X") * cameraRotateSpeed);
+    }
+}
