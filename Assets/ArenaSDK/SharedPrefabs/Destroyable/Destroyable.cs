3e51d6f2132d02c7cdad9ff6a171c296ac058979
Yuhang Song <yuhangsong.china@gmail.com>
2019-06-30 12:45:42 +0100

    clean up

---
+++ Assets/ArenaSDK/SharedPrefabs/Destroyable/Destroyable.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace Arena
+{
+    public class Destroyable : MonoBehaviour
+    {
+        public string DestoryerByTag = "Bullet";
+
+        public int LifeTotal = 1;
+
+        public bool EnableExploreEffect;
+        public GameObject ExplosionEffect;
+
+        private int NumLife;
+
+        void
+        Start()
+        {
+            this.NumLife = this.LifeTotal;
+        }
+
+        void
+        OnEnable()
+        {
+            this.NumLife = this.LifeTotal;
+        }
+
+        void
+        OnCollisionEnter(Collision other)
+        {
+            if (this.LifeTotal > 0) {
+                if (other.gameObject.CompareTag(this.DestoryerByTag)) {
+                    if (EnableExploreEffect) {
+                        GameObject ExplosionEffectPuff =
+                          Instantiate(ExplosionEffect, other.transform.position,
+                            other.transform.rotation) as GameObject;
+                        Destroy(ExplosionEffectPuff, 2);
+                    }
+
+                    this.NumLife -= 1;
+                    if (this.NumLife == 0) {
+                        this.gameObject.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+}
