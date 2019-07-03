9a8370f1e49557aaa5f3d5e7a11b44b48b3540da
Yuhang Song <yuhangsong.china@gmail.com>
2019-07-03 02:53:00 +0100

    clean up for UI percentage bar

--- Assets/ArenaSDK/SharedPrefabs/UIPercentageBar/UIPercentageBar.cs
+++ Assets/ArenaSDK/SharedPrefabs/UIPercentageBar/UIPercentageBar.cs
@@ -3,17 +3,47 @@ using UnityEngine.UI;
 
 public class UIPercentageBar : MonoBehaviour
 {
-    public string ID = "Value";
+    public string ID = "ID";
 
     public Text TextOnDisplay;
     public Image ImageOnDisplay;
 
     float CurrentPercentage = 0f;
+    bool Enabled = true;
 
     void
     Start()
     {
-        UpdatePercentage(0.323f);
+        // by default, it is disabled
+        Disable();
+    }
+
+    /// <summary>
+    /// Enable this bar, this will cause:
+    ///   text display: "ID: Enabled"
+    /// </summary>
+    public void
+    Enable()
+    {
+        if (!Enabled) {
+            Enabled = true;
+            TextOnDisplay.text        = string.Format("{0}: {1} %", ID, "Enabled");
+            ImageOnDisplay.fillAmount = 0f;
+        }
+    }
+
+    /// <summary>
+    /// Disable this bar, this will cause:
+    ///   text display: "ID: Disabled"
+    /// </summary>
+    public void
+    Disable()
+    {
+        if (Enabled) {
+            Enabled = false;
+            TextOnDisplay.text        = string.Format("{0}: {1} %", ID, "Disabled");
+            ImageOnDisplay.fillAmount = 0f;
+        }
     }
 
     /// <summary>
@@ -23,12 +53,18 @@ public class UIPercentageBar : MonoBehaviour
     public void
     UpdatePercentage(float Percentage_)
     {
-        if (Percentage_ != CurrentPercentage) {
-            CurrentPercentage = Percentage_;
+        if (Enabled) {
+            if (Percentage_ != CurrentPercentage) {
+                CurrentPercentage = Percentage_;
 
-            // update
-            TextOnDisplay.text        = string.Format("{0}: {1} %", ID, Mathf.RoundToInt(CurrentPercentage * 100f));
-            ImageOnDisplay.fillAmount = CurrentPercentage;
+                // update
+                TextOnDisplay.text        = string.Format("{0}: {1} %", ID, Mathf.RoundToInt(CurrentPercentage * 100f));
+                ImageOnDisplay.fillAmount = CurrentPercentage;
+            }
+        } else {
+            Debug.LogWarning(
+                "UIPercentageBar with ID " + ID
+                + " is disabled, call Enable() to initialize before calling UpdatePercentage()");
         }
     }
 }
