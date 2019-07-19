59e2dca21df970fd6e560e2b090ba466c1258019
Yuhang Song <yuhangsong.china@gmail.com>
2019-07-19 14:55:49 +0100

    add snake 2p game

---
+++ Assets/ArenaSDK/AgentPrefabs/SnakeAgent/SnakeHead.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arena
+{
+    public class SnakeHead : Gate
+    {
+        protected override void
+        TrigEvent(GameObject other)
+        {
+            // if collid with body
+            if (other.CompareTag("Body")) {
+                ArenaNode OtherNode = Utils.GetBottomLevelArenaNodeInGameObject(other);
+                ArenaNode ThisNode  = Utils.GetBottomLevelArenaNodeInGameObject(gameObject);
+                List<int> ThisNodeCoordinate  = ThisNode.GetCoordinate();
+                List<int> OtherNodeCoordinate = OtherNode.GetCoordinate();
+                if (!Utils.IsListEqual(ThisNodeCoordinate, OtherNodeCoordinate,
+                  Mathf.Min(ThisNodeCoordinate.Count, OtherNodeCoordinate.Count)))
+                {
+                    ThisNode.Kill();
+                }
+            }
+        } // TrigEvent
+    }
+}
