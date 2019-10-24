47b6cda34ccc3e6f60015471453f1f0a54ee9053
Andrzej Wójcicki <anonymous>
2019-10-24 14:55:06 +0200

    Lidar script with in-editor preview

--- Assets/ArenaSDK/SharedScripts/Lidar.cs
+++ Assets/ArenaSDK/SharedScripts/Lidar.cs
@@ -2,101 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace Arena
 {
+    [RequireComponent(typeof(LidarCast))]
     public class Lidar : ArenaBase
     {
-        /// <summary>
-        /// Within this bound, lidar returns 0
-        /// </summary>
-        public GameObject InnerBound;
-
-        /// <summary>
-        /// Exceed this bound, lidar returns 1
-        /// </summary>
-        public GameObject OutterBound;
-
-        /// <summary>
-        /// Direction to the front
-        /// </summary>
-        public GameObject Front;
-
-        /// <summary>
-        /// Within this bound, lidar returns 0
-        /// </summary>
-        private float InnerBoundR;
-
-        /// <summary>
-        /// Exceed this bound, lidar returns 1
-        /// </summary>
-        private float OutterBoundR;
-
-        /// <summary>
-        /// Direction to the front
-        /// </summary>
-        private Vector3 FrontV;
-
-        public override void
-        Initialize()
+        public float[] GetFlattenFrame()
         {
-            base.Initialize();
-
-            // Debug.Log(GetLogTag() + " Initialize");
-        }
-
-        private void
-        CheckConfig()
-        {
-            InnerBoundR  = GetBoundRFromBound(InnerBound);
-            OutterBoundR = GetBoundRFromBound(OutterBound);
-            FrontV       = (Front.transform.position - transform.position)
-              / (Front.transform.position - transform.position).magnitude;
-        }
-
-        private float
-        GetBoundRFromBound(GameObject Bound)
-        {
-            float BoundR;
-
-            if ((Bound.transform.lossyScale / Bound.transform.lossyScale.magnitude).Equals(Utils.Ones)) {
-                BoundR = Bound.transform.lossyScale.magnitude;
-            } else {
-                Debug.LogError(GetLogTag() + " Bound " + Bound + " is not Ones.");
-                BoundR = 0f;
-            }
-            return BoundR;
-        }
-
-        /// <summary>
-        /// Get lidar frame (in its original shape: h*w).
-        /// This is essitially a depth map
-        /// </summary>
-        public float[,]
-        GetFrame()
-        {
-            float[,] x = new float[10, 10];
-
-            return x;
-        }
-
-        /// <summary>
-        /// Get lidar frame in a flatten float array.
-        /// </summary>
-        public float[]
-        GetFlattenFrame()
-        {
-            float[] x = new float[10];
-            return x;
-        }
-
-        /// <summary>
-        /// Get the log tag of the object.
-        /// </summary>
-        /// <returns>LogTag.</returns>
-        public override string
-        GetLogTag()
-        {
-            return GetComponentInParent<ArenaAgent>().GetLogTag() + "-Lidar";
+            return gameObject.GetComponent<LidarCast>().LidarValue;
         }
     }
 }
---
+++ Assets/ArenaSDK/SharedScripts/LidarCast.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class LidarCast : MonoBehaviour
+{
+
+    [Range(1, 128)]
+    public int h = 16;           // number of collumns
+    [Tooltip("Field-of-View in degrees")]
+    [Range(1f, 359f)]
+    public float H_FOV = 150;     // Horizontal field of view 
+    [Tooltip("Range as a distance from Lidars position")]
+    [Range(0.1f, 50f)]
+    public float Range = 5;      // range of the lidar in scene units
+    [Range(0.0f, 2f)]
+    [Tooltip("Starting offset used to avoid selfcollision with the agent")]
+    public float Offset = 0.5f;
+    public Color Raycolor = new Color(0.7f, 0.81f, 0.40f, 1f);
+
+    List<Vector3> RaysList = new List<Vector3>();
+
+    RaycastHit Hit;
+    [HideInInspector]
+    public float[] LidarValue;          // Array used as vector observations 
+
+    void Update()
+    {
+        if (!EditorApplication.isPlaying)
+        {
+            PopulateRays(h, H_FOV);
+            DrawRays();
+        }
+        else
+        {
+            for (int i = 0; i < RaysList.Count; i++)
+            {
+                LidarValue[i] = CastRay(RaysList[i]);
+            }
+        }
+    }
+    void DrawRays() //used for preview in editor
+    {
+        foreach (Vector3 ray in RaysList)
+        {
+            Vector3 origin = transform.position + ray * Offset;
+            Debug.DrawRay(origin, ray * (Range - Offset), Raycolor);
+        }
+    }
+    void PopulateRays(int hcount, float fov)
+    {
+
+        RaysList.Clear();
+        Vector3 currentRay;
+        Vector3 fwd = gameObject.transform.forward;
+        float stepAngle = fov / hcount;
+
+        for (int i = 0; i < hcount; i++)
+        {
+            for (int j = 0; j < hcount; j++)
+            {
+                currentRay = Quaternion.Euler(0, (-fov / 2) + stepAngle / 2 + stepAngle * (i), 0) * fwd;
+
+                currentRay = currentRay.normalized;
+                RaysList.Add(currentRay);
+            }
+        }
+
+    }
+
+    private float CastRay(Vector3 ray) // returns the distance to the closest hit of raycast
+    {
+        Vector3 origin = transform.position + ray * Range / Offset;
+        if (Physics.Raycast(origin, ray, Range - Offset))            //  add a layer mask value if you need to ignore certain type of objects
+        {
+            float distance = Vector3.Distance(Hit.transform.position, transform.position);
+            return distance;
+        }
+        else return 0f;
+    }
+}
