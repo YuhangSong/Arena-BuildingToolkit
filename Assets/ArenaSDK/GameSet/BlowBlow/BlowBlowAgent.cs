30c4b16216cd57a4819414524c70a9b0c65487b9
YuhangSong <YuhangSong2017@gmail.com>
2019-10-28 10:06:17 +0800

    clean up

--- Assets/ArenaSDK/GameSet/BlowBlow/BlowBlowAgent.cs
+++ Assets/ArenaSDK/GameSet/BlowBlow/BlowBlowAgent.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 namespace Arena
 {
     public class BlowBlowAgent : BasicAgent {
+        public int NumBulletPerRelease = 3;
         public GameObject Bullet_Emitter;
-        public GameObject Bullet_Emitter1;
-        public GameObject Bullet_Emitter2;
+        public GameObject Bullet_EmitterLeftEdge;
+        public GameObject Bullet_EmitterRightEdge;
 
         override protected void
         DiscreteStep(int Action_)
@@ -14,19 +15,20 @@ namespace Arena
 
             switch (Action_) {
                 case Forward:
-                    for (int i = 0; i < 2; i++) {
+                    for (int i = 0; i < NumBulletPerRelease; i++) {
                         GameObject Temp_Bullet_Handeler;
                         Temp_Bullet_Handeler = Instantiate(Bullet, Bullet_Emitter.transform.position,
                             Bullet_Emitter.transform.rotation) as GameObject;
                         var vs =
-                          Vector3.Normalize(Vector3.Slerp((Bullet_Emitter1.transform.position
+                          Vector3.Normalize(Vector3.Slerp((Bullet_EmitterLeftEdge.transform.position
                             - Bullet_Emitter.transform.position),
-                            (Bullet_Emitter2.transform.position - Bullet_Emitter.transform.position), Random.value));
+                            (Bullet_EmitterRightEdge.transform.position - Bullet_Emitter.transform.position),
+                            Random.value));
                         Temp_Bullet_Handeler.GetComponent<Rigidbody>().velocity = vs * 10f;
                         Destroy(Temp_Bullet_Handeler, 3.0f);
                     }
                     Player.GetComponentInChildren<Rigidbody>().AddForce(Player.transform.TransformVector(
-                          Vector3.right * 40f));
+                          Vector3.forward * 40f));
                     Player.GetComponentInChildren<Rigidbody>().angularVelocity = Vector3.zero;
                     break;
                 default:
